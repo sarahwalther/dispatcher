9a5d80003047d992ecc77f4b85da491671579905
Sarah <sarah.mcalear@gmail.com>
2021-03-13 12:06:23 -0500

    Initial commit, getting base spring project

---
+++ src/test/kotlin/com/css/dispatcher/DispatcherApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.css.dispatcher
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class DispatcherApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
